2438878393dfb98fd6d9ddd3a75cf317e15c4c7c
Parul <parsingh@redhat.com>
2022-02-16 18:22:31 -0500

    crictl useful commands

---
+++ crictl_commands.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# systemctl stop kubelet
+# crictl stopp `crictl pods -q`        ##  "stopp" with two "p" for stopping pods
+# crictl stop `crictl ps -aq`
+# crictl rmp `crictl pods -q`
+# rm -rf /var/lib/containers/*
+# crio wipe -f
