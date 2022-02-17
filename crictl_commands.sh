cd43a880014936bf738887b49ca53aafdbb6d235
Parul <parsingh@redhat.com>
2022-02-17 15:09:12 -0500

    mount|umount

--- crictl_commands.sh
+++ crictl_commands.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # systemctl stop kubelet
@@ -6,3 +7,6 @@
 # crictl rmp `crictl pods -q`
 # rm -rf /var/lib/containers/*
 # crio wipe -f
+sudo mount | grep  </path>
+#/dev/nvme0n1p3 on /root/rpmbuild/BUILDROOT/microshift-images-1-1.x86_64/var/lib/microshift/images/overlay
+#sudo umount -lf  /dev/nvme0n1p3
